89c321573f619df97c11363237193fcbe24e34cc
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-01-12 15:47:12 -0300

    updates in autommation

--- projeto/automacao.py
+++ projeto/automacao.py
@@ -1,43 +1,52 @@
-from cores import(Negrito,Reset,Verde,Amarelo)
+from cores import(Negrito,Reset,Verde,Amarelo,MagentaClaro)
 import os
 import pyautogui
 from time import sleep
-import pandas as pd
+import pandas as pandas
 import pyperclip
 import openpyxl
 
 os.system("open -a 'Google Chrome'")
 sleep(2)
-pyautogui.write("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga")
+
+drive_dados = "https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga"
+
+pyautogui.write(drive_dados)
 
 pyautogui.press("enter")
 sleep(2)
-pyautogui.click(x=260, y=230)
+pyautogui.click(x=150, y=230)
 sleep(3)
 pyautogui.click(x=300, y=300)
+sleep(4)
+pyautogui.press("enter")
 sleep(4)
 pyautogui.press("enter")
 
 caminho_rota = (r"/Users/lucaspaguettipereira/Downloads/Vendas.xlsx") #atente-se em mudar o caminho do seu arquivo
 
-tabela_faturamento_empresa = pd.read_excel(caminho_rota)
+tabela_faturamento_empresa = pandas.read_excel(caminho_rota)
 faturamento_empresa = tabela_faturamento_empresa["Valor Final"].sum()
 quantidade_empresa_produtos = tabela_faturamento_empresa["Quantidade"].sum()
 
 print(f"\n {Negrito}Faturamento da Empresa:{Reset} {Verde}R${faturamento_empresa:.2f}{Reset}", end=" \n ")
+
 print(f"\n {Negrito}Produtos da empresa (em quantidade): {Reset}{Amarelo}{quantidade_empresa_produtos}{Reset} Produtos 📦", end=" \n ")
 
-remetente = f"@testedeemailautomático.com"
+remetente = f"lpp2@cesar.school"
 
-assunto = f"Relatório de Vendas – Janeiro"
+assunto = f"Relatório de Vendas – Janeiro 💸📊"
 corpo = f"""Prezados,
 
-Encaminho, abaixo, o relatório de vendas referente ao mês de janeiro 👇📊
+Encaminho, abaixo, o relatório de vendas referente ao mês de janeiro 
 
 Resumo dos resultados:
 • Faturamento total: R$ {faturamento_empresa:,.2f} .
 • Quantidade de produtos vendidos: {quantidade_empresa_produtos:.2f} .
 Fico à disposição para quaisquer esclarecimentos adicionais.
+
+- Se quiserem se aprofundar dos dados, acessem:\n
+{drive_dados}
 
 Atenciosamente,
 Lucas Paguetti Pereira.
@@ -45,9 +54,11 @@ Lucas Paguetti Pereira.
 pyautogui.hotkey("command", "t")
 sleep(1)
 
-pyautogui.write("https://mail.google.com/mail/u/0/#inbox")
+acessar_gmail = "https://mail.google.com/mail/u/0/#inbox"
+
+pyautogui.write(acessar_gmail)
 pyautogui.press("enter")
-sleep(10)
+sleep(20)
 pyautogui.click(x=100, y=230)  
 sleep(2)
 
@@ -67,4 +78,14 @@ pyautogui.hotkey("command", "v")
 
 sleep(2)
 
+pyautogui.hotkey("command", "enter")
+
 print(f"\n {Negrito}E-mail{Reset} {Verde}enviado com sucesso! ✅📧🤖 {Reset}\n", end=" ")
+
+print(f"""\n {Negrito}Drive com os dados:\n{Reset}
+{MagentaClaro}Pasta dados/
+├── Exportar{Reset}
+│   {Amarelo}└── Vendas.xlsx{Reset}
+{MagentaClaro}├── Apostila - Aula1.pdf
+├── Arquivo Inicial - Aula 1.ipynb
+└── Arquivo Inicial - Aula 1.py \n\n{Reset}""")
